a8f38251e2bcecfa970eaaf9fa9e9beba6096c7c
tewedaj <tewedaj13@gmail.com>
2022-04-07 20:15:59 +0300

    almost done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ * Description: prints quote to standard error
+ * Return: 1 if program executes properly, else a non-zero number
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - starting point of the program
+ *
+ * Description: prints a sample string
+ * Return: 0 if program executes properly, else a non-zero number
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
